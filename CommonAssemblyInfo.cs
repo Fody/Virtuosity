bf4ad69b2bb27de9d580611ec6d3ef8a2d604961
Simon Cropp <simon.cropp@gmail.com>
2013-07-07 20:52:24 +1000

    support for TFS checkout

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Virtuosity")]
 [assembly: AssemblyProduct("Virtuosity")]
-[assembly: AssemblyVersion("1.19.1.1")]
-[assembly: AssemblyFileVersion("1.19.1.1")]
+[assembly: AssemblyVersion("1.19.2")]
+[assembly: AssemblyFileVersion("1.19.2")]
