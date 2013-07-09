57ab0d35615473dc60abf166960d48531c9dbffc
Simon Cropp <simon.cropp@gmail.com>
2013-07-09 22:28:33 +1000

    fix VS Guid

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Virtuosity")]
 [assembly: AssemblyProduct("Virtuosity")]
-[assembly: AssemblyVersion("1.19.2")]
-[assembly: AssemblyFileVersion("1.19.2")]
+[assembly: AssemblyVersion("1.19.3")]
+[assembly: AssemblyFileVersion("1.19.3")]
