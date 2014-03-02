229360d572cb0de5c2313e7add058686fc27f17e
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:27:35 +1100

    dev dep

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Virtuosity")]
 [assembly: AssemblyProduct("Virtuosity")]
-[assembly: AssemblyVersion("1.19.5")]
-[assembly: AssemblyFileVersion("1.19.5")]
+[assembly: AssemblyVersion("1.19.6")]
+[assembly: AssemblyFileVersion("1.19.6")]
