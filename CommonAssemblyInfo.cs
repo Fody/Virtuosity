d711422b791e170d2f922e0a8ed78c9a09707a1c
Simon Cropp <simon.cropp@gmail.com>
2015-08-16 09:46:23 +1000

    fix nuspec name

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Virtuosity")]
 [assembly: AssemblyProduct("Virtuosity")]
-[assembly: AssemblyVersion("1.19.9")]
-[assembly: AssemblyFileVersion("1.19.9")]
+[assembly: AssemblyVersion("1.19.10")]
+[assembly: AssemblyFileVersion("1.19.10")]
