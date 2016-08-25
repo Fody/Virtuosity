0d55b647e997b4b449b748c5d7e4f6518c14fba9
Marco Erni <marco.erni@vhome.ch>
2016-08-25 13:28:22 +0200

    Logging severity
    Decreased logging severity from info to debug

--- Fody/ModuleWeaver.cs
+++ Fody/ModuleWeaver.cs
@@ -3,12 +3,12 @@ using Mono.Cecil;
 
 public partial class ModuleWeaver
 {
-    public Action<string> LogInfo { get; set; }
+    public Action<string> LogDebug { get; set; }
     public ModuleDefinition ModuleDefinition { get; set; }
 
     public ModuleWeaver()
     {
-        LogInfo = s => { };
+        LogDebug = s => { };
     }
 
     public void Execute()
