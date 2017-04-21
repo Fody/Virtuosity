64f995292226d27116651b9f59fb9fee5312d52e
Simon Cropp <simon.cropp@gmail.com>
2017-04-21 12:58:33 +1000

    fix version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Virtuosity")]
 [assembly: AssemblyProduct("Virtuosity")]
-[assembly: AssemblyVersion("1.19.12")]
-[assembly: AssemblyFileVersion("1.19.12")]
+[assembly: AssemblyVersion("1.20.0")]
+[assembly: AssemblyFileVersion("1.20.0")]
