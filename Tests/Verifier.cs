de78e4dbca7320dec28c6e7a086a2ce85ddec74b
Bruno Juchli <bruno.juchli@innocreative.ch>
2017-09-26 08:28:12 +0200

    -updated PEVerify logic (copied from PropertyChanged.Fody) -added test case for issue #14 (https://github.com/Fody/Virtuosity/issues/14)

--- Tests/Verifier.cs
+++ Tests/Verifier.cs
@@ -1,26 +1,44 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.Build.Utilities;
 using NUnit.Framework;
 
 public static class Verifier
 {
+    static string exePath;
+    static bool peverifyFound;
+
+    static Verifier()
+    {
+        var sdkPath = Path.GetFullPath(Path.Combine(ToolLocationHelper.GetPathToDotNetFrameworkSdk(TargetDotNetFrameworkVersion.VersionLatest), "..\\.."));
+        exePath = Directory.GetFiles(sdkPath, "peverify.exe", SearchOption.AllDirectories).LastOrDefault();
+
+        peverifyFound = File.Exists(exePath);
+        if (!peverifyFound)
+        {
+#if(!DEBUG)
+            throw new System.Exception("Could not find PEVerify");
+#endif
+        }
+    }
     public static void Verify(string beforeAssemblyPath, string afterAssemblyPath)
     {
+        if (!peverifyFound)
+        {
+            return;
+        }
+        Debug.WriteLine(afterAssemblyPath);
         var before = Validate(beforeAssemblyPath);
         var after = Validate(afterAssemblyPath);
         var message = $"Failed processing {Path.GetFileName(afterAssemblyPath)}\r\n{after}";
         Assert.AreEqual(TrimLineNumbers(before), TrimLineNumbers(after), message);
     }
 
-    static string Validate(string assemblyPath2)
+    public static string Validate(string assemblyPath2)
     {
-        var exePath = GetPathToPEVerify();
-        if (!File.Exists(exePath))
-        {
-            return string.Empty;
-        }
+
         var process = Process.Start(new ProcessStartInfo(exePath, "\"" + assemblyPath2 + "\"")
         {
             RedirectStandardOutput = true,
@@ -30,17 +48,6 @@ public static class Verifier
 
         process.WaitForExit(10000);
         return process.StandardOutput.ReadToEnd().Trim().Replace(assemblyPath2, "");
-    }
-
-    static string GetPathToPEVerify()
-    {
-        var exePath = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\PEVerify.exe");
-
-        if (!File.Exists(exePath))
-        {
-            exePath = Environment.ExpandEnvironmentVariables(@"%programfiles(x86)%\Microsoft SDKs\Windows\v8.0A\Bin\NETFX 4.0 Tools\PEVerify.exe");
-        }
-        return exePath;
     }
 
     static string TrimLineNumbers(string foo)
