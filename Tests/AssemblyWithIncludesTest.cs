ea7aaa899a4b0aaffd764eb67b536d5a4248b51b
simoncropp <simon.cropp@gmail.com>
2019-03-24 09:00:01 +1100

    Update AssemblyWithIncludesTest.cs

--- Tests/AssemblyWithIncludesTest.cs
+++ Tests/AssemblyWithIncludesTest.cs
@@ -1,25 +1,29 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using Fody;
 using Xunit;
 
 public class AssemblyWithIncludesTest
 {
-    [Fact]
-    public void Simple()
+    static Assembly assembly;
+
+    static AssemblyWithIncludesTest()
     {
         var weavingTask = new ModuleWeaver
         {
             IncludeNamespaces = new List<string> { "IncludeNamespace" }
         };
-        var assembly = weavingTask.ExecuteTestRun("AssemblyWithIncludes.dll").Assembly;
-        var excludeType = assembly.GetType("ExcludeNamespace.ExcludeClass");
-        Assert.False(excludeType.GetMethod("Method").IsVirtual);
-        var includeType = assembly.GetType("IncludeNamespace.IncludeClass");
-        Assert.True(includeType.GetMethod("Method").IsVirtual);
+        assembly = weavingTask.ExecuteTestRun("AssemblyWithIncludes.dll",
+            assemblyName: nameof(IntegrationTests)).Assembly;
+    }
 
-        var inNamespaceButWithAttributeType = assembly.GetType("IncludeNamespace.InNamespaceButWithAttributeClass");
-        Assert.False(inNamespaceButWithAttributeType.GetMethod("Method").IsVirtual);
-        var notInNamespaceButWithAttributeType = assembly.GetType("ExcludeNamespace.NotInNamespaceButWithAttributeClass");
-        Assert.False(notInNamespaceButWithAttributeType.GetMethod("Method").IsVirtual);
+    [Fact]
+    public void Simple()
+    {
+        VirtualTester.EnsureMembersAreNotVirtual("ExcludeNamespace.ExcludeClass", assembly, "Method");
+        VirtualTester.EnsureMembersAreVirtual("IncludeNamespace.IncludeClass", assembly, "Method");
+        VirtualTester.EnsureMembersAreNotVirtual("IncludeNamespace.InNamespaceButWithAttributeClass", assembly, "Method");
+        VirtualTester.EnsureMembersAreNotVirtual("ExcludeNamespace.NotInNamespaceButWithAttributeClass", assembly, "Method");
+    }
     }
 }
